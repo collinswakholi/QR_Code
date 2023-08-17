61cddc16c5eb6181d452ba07ec71321a5064f211
Wakholi Collins <49397327+collinswakholi@users.noreply.github.com>
2023-08-17 12:43:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+    long_descr = fh.read()
     
 with open("requirements.txt", "r") as f:
-    requirements = f.readlines()
+    req = f.readlines()
     
 name_ = "readQR"
 desc_ = "Read QR code from image using WeChat QR code reader in OpenCV"
@@ -12,14 +12,14 @@ setup(
     name=name_,
     version="0.1.0",
     description=desc_,
-    long_description=long_description,
+    long_description=long_descr,
     long_description_content_type="text/markdown",
     author="Collins Wakholi",
-    url="https://github.com/CollinsWakholi/readQR_weChat",
+    url="https://github.com/CollinsWakholi/QR_Code",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=req,
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
