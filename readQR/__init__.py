8cda3d1a6a990ac72ed698b4a75da0ee48ad70e4
Wakholi Collins <49397327+collinswakholi@users.noreply.github.com>
2023-08-18 11:00:30 -0700

    Update __init__.py

--- readQR/__init__.py
+++ readQR/__init__.py
@@ -1,8 +1,8 @@
 import sys
+import os
 from readQR.readQR import readQR_code
-
-
 from pkg_resources import get_distribution, DistributionNotFound
+
 try:
     version_ = get_distribution(__name__).version
 except DistributionNotFound:
@@ -14,3 +14,15 @@ __all__ = ["readQR_code"]
 if "pdoc" in sys.modules:
     with open("README.md", "r") as fh:
         _readme = fh.read()
+
+package_dir = os.path.dirname(__file__)
+artefact_dir = os.path.join(package_dir,"wechat_artefacts")
+
+artefacts = os.listdir(artefact_dir)
+
+for file_name in artefacts:
+    if file_name.endswith(".prototxt") or file_name.endswith(".caffemodel"):
+        file_path = os.path.join(artefact_dir, file_name)
+        model_var_name = "model_"+file_name.replace(".","_")
+        globals()[model_var_name] = file_path
+        __all__.append(model_var_name)
